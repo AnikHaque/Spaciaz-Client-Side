563f52f17e989a9811725128c442c27f28fc7f88
AnikHaque <ekramul15-811@diu.edu.bd>
2025-06-16 03:20:00 +0600

    about content done

---
+++ src/components/About/AboutContent.jsx
@@ -0,0 +1,42 @@
+const AboutContent = () => {
+  return (
+    <div>
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="container px-6 py-16 mx-auto">
+          <div class="items-center lg:flex">
+            <div class="w-full lg:w-1/2">
+              <div class="lg:max-w-lg">
+                <h1 class="text-3xl font-bold text-gray-800  lg:text-5xl mb-8">
+                  Shaping the world of things to come{" "}
+                </h1>
+
+                <p class="mt-3 text-gray-600">
+                  In markets from renewable energy, sports and entertainment, to
+                  data centers and healthcare, we work to ensure the built
+                  environment leaves a lasting positive impact. Together, we
+                  strive to make your project better than you imagined possible.
+                </p>
+                <p class="mt-3 text-gray-600">
+                  In markets from renewable energy, sports and entertainment, to
+                  data centers and healthcare, we work to ensure the built
+                  environment leaves a lasting positive impact. Together, we
+                  strive to make your project better than you imagined possible.
+                </p>
+              </div>
+            </div>
+
+            <div class="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
+              <img
+                class="w-[700px] h-[500px] rounded-xl"
+                src="https://demo2.wpopal.com/spaciaz/wp-content/uploads/2025/03/h2_img1.jpg"
+                alt="email illustration vector art"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutContent;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AboutBanner from "../components/About/AboutBanner";
+import AboutContent from "../components/About/AboutContent";
 
 const About = () => {
   return (
     <div>
       <AboutBanner></AboutBanner>
+      <AboutContent></AboutContent>
     </div>
   );
 };
